e4d6bd57312337337bae749b51fdd4d48f5817f9
kerolos-nader24 <12kero88kero@gmail.com>
2024-08-07 15:51:04 +0300

    create my project

--- FirstTerm/Unit_2_C_Programming/C_Pointers/p ahpha.c
+++ FirstTerm/Unit_2_C_Programming/C_Pointers/p ahpha.c
@@ -8,3 +8,33 @@
  ============================================================================
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+char str1 [27];
+char *ptr ;
+int x;
+
+ptr=str1;
+for(x=0;x<26;x++){
+	*ptr=x+'A';
+	ptr++;
+
+
+}
+ptr=str1;
+for(x=0;x<26;x++){
+	printf("%c",*ptr);
+
+	ptr++;
+
+}
+printf("\n\n");
+    return 0;
+}
+
+
